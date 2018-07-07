1be693954f467f3f915592b31541e3cef26db942
shotat <shotat@users.noreply.github.com>
2018-07-07 23:49:33 +0900

    Carthage

--- STKImagePicker/AppDelegate.swift
+++ STKImagePicker/AppDelegate.swift
@@ -10,12 +10,14 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
