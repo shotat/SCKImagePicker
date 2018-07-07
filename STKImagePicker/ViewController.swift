8d721242b04be44da5da0076557612b87264fed8
shotat <shotat@users.noreply.github.com>
2018-07-07 23:38:29 +0900

    Initial Commit

---
+++ STKImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  STKImagePicker
+//
+//  Created by Shota Terashita on 2018/07/07.
+//  Copyright © 2018年 Shotat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
