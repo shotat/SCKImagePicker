0500586ae7e0fbe1680414c960e585c1697a7e1e
shotat <shotat@users.noreply.github.com>
2018-07-08 19:04:59 +0900

    add manager

---
+++ SCKImagePicker/Presenters/SCKAlbumsManager.swift
@@ -0,0 +1,70 @@
+import Foundation
+import Photos
+
+struct SCKAlbum {
+    var thumbnail: UIImage?
+    var title: String = ""
+    var numberOfItems: Int = 0
+    var collection: PHAssetCollection?
+}
+
+class SCKAlbumsManager {
+    private static let instance = SCKAlbumsManager()
+    class var sharedDefault: SCKAlbumsManager {
+        return instance
+    }
+
+    private var cachedAlbums: [SCKAlbum]?
+
+    func fetchAlbums() -> [SCKAlbum] {
+        if let cachedAlbums = cachedAlbums { return cachedAlbums }
+        var albums = [SCKAlbum]()
+        let options = PHFetchOptions()
+
+        let smartAlbumsResult = PHAssetCollection.fetchAssetCollections(with: .smartAlbum,
+                                                                        subtype: .any,
+                                                                        options: options)
+        let albumsResult = PHAssetCollection.fetchAssetCollections(with: .album,
+                                                                   subtype: .any,
+                                                                   options: options)
+        for result in [smartAlbumsResult, albumsResult] {
+            result.enumerateObjects({ assetCollection, _, _ in
+                var album = SCKAlbum()
+                album.title = assetCollection.localizedTitle ?? ""
+                album.numberOfItems = self.mediaCountFor(collection: assetCollection)
+                if album.numberOfItems > 0 {
+                    let r = PHAsset.fetchKeyAssets(in: assetCollection, options: nil)
+                    if let first = r?.firstObject {
+                        let targetSize = CGSize(width: 78 * 2, height: 78 * 2)
+                        let options = PHImageRequestOptions()
+                        options.isSynchronous = true
+                        options.deliveryMode = .fastFormat
+                        PHImageManager.default().requestImage(for: first,
+                                                              targetSize: targetSize,
+                                                              contentMode: .aspectFit,
+                                                              options: options,
+                                                              resultHandler: { image, _ in
+                                                                  album.thumbnail = image
+                        })
+                    }
+                    album.collection = assetCollection
+
+                    if !(assetCollection.assetCollectionSubtype == .smartAlbumSlomoVideos
+                        || assetCollection.assetCollectionSubtype == .smartAlbumVideos) {
+                        albums.append(album)
+                    }
+                }
+            })
+        }
+        cachedAlbums = albums
+        return albums
+    }
+
+    func mediaCountFor(collection: PHAssetCollection) -> Int {
+        let options = PHFetchOptions()
+        let photoPredicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
+        options.predicate = photoPredicate
+        let result = PHAsset.fetchAssets(in: collection, options: options)
+        return result.count
+    }
+}
